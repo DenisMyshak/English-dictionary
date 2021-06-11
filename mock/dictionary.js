6a0e89c80e7dde94f510b7d49e5097c05dfaedc9
dm25an22 <dm25an22@gmail.com>
2021-06-11 23:51:44 +0200

    fix search

--- mock/dictionary.js
+++ mock/dictionary.js
@@ -1,24 +1,5 @@
 export const words = [
-    {
-        id: 3,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
+    
     {
         id: 1,
         name: `hello`
@@ -39,204 +20,204 @@ export const words = [
         id: 5,
         name: `excellent`
     },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    },
-    {
-        id: 1,
-        name: `hello`
-    },
-    {
-        id: 2,
-        name: `thanks`
-    },
-    {
-        id: 3,
-        name: `bird`
-    },
-    {
-        id: 4,
-        name: `good`
-    },
-    {
-        id: 5,
-        name: `excellent`
-    }
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // },
+    // {
+    //     id: 1,
+    //     name: `hello`
+    // },
+    // {
+    //     id: 2,
+    //     name: `thanks`
+    // },
+    // {
+    //     id: 3,
+    //     name: `bird`
+    // },
+    // {
+    //     id: 4,
+    //     name: `good`
+    // },
+    // {
+    //     id: 5,
+    //     name: `excellent`
+    // }
 ];
